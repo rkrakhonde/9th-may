6567109061108134749872b3501e610187031f2a
krakh <rohinikhapre@gmail.com>
2026-07-02 19:34:41 +0530

    commit

---
+++ polymorphism/sample1.java
@@ -0,0 +1,18 @@
+package polymorphism;
+
+public class sample1 {
+   //method overloading
+   //method with 2 int parameter
+   public void add(int num1, int num2) {
+      System.out.println(num1 + num2);
+   }
+
+   //method with 3 int parameters
+   public void add(int num1, int num2, int num3) {
+      System.out.println(num1 + num2 + num3);
+   }
+}
+
+
+
+
